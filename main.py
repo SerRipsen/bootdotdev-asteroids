78e0ea2affa77864569469d07801b35e1e1bee8f
Ser Ripsen <ser@ripsen.com>
2025-11-30 15:24:51 +0000

    Added: Basic WASD movement to the player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 from logger import log_state
 
 def main():
@@ -7,18 +8,35 @@ def main():
           Screen width: {SCREEN_WIDTH}
           Screen height: {SCREEN_HEIGHT}""")
 
+    # initialise basic info
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # create groups of updatable, drawable
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    # create player
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    # main game loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # render the screen items
         screen.fill("black")
+        updatable.update(dt)
+        for _draw in drawable:
+            _draw.draw(screen)
+            
         
-        # set game speed to 60fps
+        # set game speed to 60fps and update the clock
         dt = game_clock.tick(60) / 1000
 
         # refresh the screen
